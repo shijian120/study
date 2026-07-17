959f858182f7eefa3cd0bbbdbe10c25018043e64
shijian <596626897@qq.com>
2026-07-17 15:01:43 +0800

    延迟加载

--- ssm-parent/mybatis-01-helloword/src/test/java/com/sj/StepMapperTest.java
+++ ssm-parent/mybatis-01-helloword/src/test/java/com/sj/StepMapperTest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * ClassName: StepMapperTest
@@ -30,7 +31,7 @@ public class StepMapperTest {
 	// 按照id查询客户,以及客户的所有订单
 	// 原生分步的方式
 	@Test
-	public void test01(){
+	public void test01() {
 		// 第一步,先查询客户
 		Customer customer = stepMapper.selectCustomerById(1L);
 		
@@ -54,8 +55,32 @@ public class StepMapperTest {
 	
 	// 查询订单,以及下订单的客户
 	@Test
-	public void test03(){
+	public void test03() {
 		Order order = stepMapper.selectOrderAndCustomer(1L);
 		System.out.println("order = " + order);
+	}
+	
+	
+	// 延迟加载
+	/*
+	
+	mybatis.configuration.lazy-loading-enabled=true
+	mybatis.configuration.aggressive-lazy-loading=false
+	 */
+	@Test
+	public void test04() throws InterruptedException {
+		
+		
+		Customer customer = stepMapper.selectCustomerAndOrders(1);
+		// 值获取了 客户的名字, 没用获取客户的订单信息,此时不会发送分步查询的第二步
+		System.out.println(customer.getCustomerName());
+		
+		TimeUnit.SECONDS.sleep(3);
+		
+		// 只有用到了才会加载,才会执行分步查询的第二步
+		System.out.println(customer.getOrders());
+		
+		
+		
 	}
 }
