f70c80a5dfa2e0536eae0bc69839dd53075b6be8
shijian <596626897@qq.com>
2026-07-31 16:53:48 +0800

    nettydemo添加

---
+++ netty-demo/src/main/java/com/sj/Main.java
@@ -0,0 +1,18 @@
+package com.sj;
+
+/**
+ * ClassName: ${NAME}
+ * Package: ${Package_name}
+ * Description:
+ *
+ * @Author shijian
+ * @Create 2026/7/31 16:44
+ * @Version 1.0
+ */
+
+public class Main {
+	public static void main(String[] args) {
+
+		
+	}
+}
